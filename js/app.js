45d5b0db09e9ffcea9c04fc79d851eca24aad933
ZobayerSakib <zobayersakib44@gmail.com>
2021-09-13 12:32:01 +0600

    Alhamdulillah almost okey

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -60,12 +60,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  // document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.round((total + Number.EPSILON) * 100) / 100;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  // document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.round((value + Number.EPSILON) * 100) / 100;
 };
 
 // update delivery charge and total Tax
@@ -89,5 +91,9 @@ const updateTaxAndCharge = () => {
 const updateTotal = () => {
   const grandTotal = getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  // const totalAmount = document.getElementById("total").innerText = grandTotal;
+  const totalAmount = document.getElementById("total").innerText = Math.round((grandTotal + Number.EPSILON) * 100) / 100;;
+
+
+
 };
